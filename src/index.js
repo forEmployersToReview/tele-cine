108f600cb7f9d587357f6cf89dcc46f1de3c0513
Elifaz Filho <zafile3@hotmail.com>
2018-01-11 04:32:29 -0800

    [feat] Env setup, config and packages, initialized components.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './containers/app';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+registerServiceWorker();
